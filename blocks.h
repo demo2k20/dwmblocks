0f0bb3987f52614f9a458a772ef5845b1641d4d2
Roland Nemes <nemesroland2@gmail.com>
2022-01-15 19:20:32 +0200

    Autocommit.

--- blocks.h
+++ blocks.h
@@ -1,18 +1,17 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-//	{"",        "song",         5,                  13},
-//	{"",        "keyboard",     -1,                 12},
-//	{"",        "disk",         300,                11},
-//	{"",        "temperature",  60,                 10},
-//	{"",        "cpu",          2,                  9},
-//	{"",        "memory",       120,                8},
-//	{"",        "dod",          1,                  7},
-	{"",        "torrents",     300,                6},
-	{"",        "internet",     120,                5},
-	{"",        "brightness",   -1,                 4},
-	{"",        "volume",       -1,                 3},
-	{"",        "battery2",     120,                2},
+//	{"",        "music",        5,                  12},
+//	{"",        "keyboard",     0,                  11},
+//	{"",        "disk",         300,                10},
+//	{"",        "temperature",  60,                 9},
+//	{"",        "cpu",          2,                  8},
+//	{"",        "memory",       120,                7},
+//	{"",        "dod",          1,                  6},
+	{"",        "torrents",     300,                5},
+	{"",        "internet",     120,                4},
+	{"",        "volume",       0,                  3},
+	{"",        "battery",      120,                2},
 	{"",        "datetime",     60,                 1},
 };
 
